9614a11088b57219059824a84b804ed81a9f7c74
Franck Guadagnini <FranckiMac@mmCC.local>
2015-04-06 14:49:02 +0200

    publication

--- search/tipuesearch/tipuesearch_set.js
+++ search/tipuesearch/tipuesearch_set.js
@@ -1,13 +1,1 @@
-/*
-Tipue Search 4.0
-Copyright (c) 2014 Tipue
-Tipue Search is released under the MIT License
-http://www.tipue.com/search
-*/
-var tipuesearch_stop_words = ["les"];
-var tipuesearch_replace = {"words": [
-     {"word": "Frank", "replace_with": "Franck"}
-]};
-var tipuesearch_stem = {"words": [
-     {"word": "e-mail", "stem": "email"},
-]};
+var tipuesearch_stop_words=["les"];var tipuesearch_replace={words:[{word:"Frank",replace_with:"Franck"}]};var tipuesearch_stem={words:[{word:"e-mail",stem:"email"}]};
