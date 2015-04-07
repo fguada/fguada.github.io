2c712edc4ce1c59a6a0da290061d0ba4e1025768
Franck Guadagnini <franckguadagnini@me.com>
2015-04-07 20:59:35 +0200

    publication

--- search/tipuesearch/tipuesearch_set.js
+++ search/tipuesearch/tipuesearch_set.js
@@ -1,1 +1,13 @@
-var tipuesearch_stop_words=["les"];var tipuesearch_replace={words:[{word:"Frank",replace_with:"Franck"}]};var tipuesearch_stem={words:[{word:"e-mail",stem:"email"}]};
+/*
+Tipue Search 4.0
+Copyright (c) 2014 Tipue
+Tipue Search is released under the MIT License
+http://www.tipue.com/search
+*/
+var tipuesearch_stop_words = ["les"];
+var tipuesearch_replace = {"words": [
+     {"word": "Frank", "replace_with": "Franck"}
+]};
+var tipuesearch_stem = {"words": [
+     {"word": "e-mail", "stem": "email"},
+]};
